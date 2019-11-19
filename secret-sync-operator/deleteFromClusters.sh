f487d315e0ca2a8a4ae40cbe0e7096d6e979cd7c
Dave Bellagio <dbellagio@us.ibm.com>
2019-11-19 11:44:10 -0600

    DevOps #152 - deployment of operator to sync secrets

---
+++ secret-sync-operator/deleteFromClusters.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ibmcloud target -g "Dev Resources"
+
+# inst-musc-dev-qa
+ibmcloud ks cluster config bl5ob85d0rdfkj3mgn80
+export KUBECONFIG=/Users/dbellagio/.bluemix/plugins/container-service/clusters/bl5ob85d0rdfkj3mgn80/kube-config-dal12-inst-musc-dev-qa.yml
+# kubectl delete -f deploy/all-in-one.yaml
+
+# inst-ecu-dev-qa
+ibmcloud ks cluster config 85db7434d8ff47778d470762c64c9346
+export KUBECONFIG=/Users/dbellagio/.bluemix/plugins/container-service/clusters/85db7434d8ff47778d470762c64c9346/kube-config-dal13-inst-ecu-dev-qa.yml
+kubectl delete -f deploy/all-in-one.yaml
+
+# xcomp-dev-qa
+ibmcloud ks cluster config 62f6e6454c5041479f5e8b88781bbef3
+export KUBECONFIG=/Users/dbellagio/.bluemix/plugins/container-service/clusters/62f6e6454c5041479f5e8b88781bbef3/kube-config-dal13-xcomp-dev-qa.yml
+kubectl delete -f deploy/all-in-one.yaml
+
+# xcomp-prod-tmp
+ibmcloud target -g "Prod Resources"
+
+ibmcloud ks cluster config 876fcecefbfd41ecb923970426819140
+export KUBECONFIG=/Users/dbellagio/.bluemix/plugins/container-service/clusters/876fcecefbfd41ecb923970426819140/kube-config-dal13-xcomp-prod-tmp.yml
+kubectl delete -f deploy/all-in-one.yaml
+
+
